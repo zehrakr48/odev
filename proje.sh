4dd07bc1f7ea191910f1e5ffd87f4877caee745e
zehrakr48 <zzehrakr48@gmail.com>
2023-01-02 14:20:24 +0300

    scriptte düzenleme yapıldı.

--- proje.sh
+++ proje.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+
 DIZIN="/home/"
 SQL_AD="proje"
-SQL_KULLANICI="postres"
+SQL_KULLANICI="postgres"
 SQL_SIFRE="0000"
 SQL_TABLO="table"
 
 while true; do
-TARIH=$(date)
-DEGİSİKLİKLER=$(inotifywait -e modify,move,create,delete --format'%w&f%e' -r $DIZIN)
-if[! -z "$DEGİSİKLİKLER"]; then
-psql -d "$SQL_AD" -U "$SQL_KULLANICI" -c "INSERT INTO $SQL_TABLO(event) VALUES ('TARIH $DEGISIKLIKLER');"
+    TARIH=$(date)
+    DEGİSİKLİKLER=$(inotifywait -e modify,move,create,delete --format'%w&f%e' -r $DIZIN)
+    if [! -z "$DEGİSİKLİKLER"]; then
+        psql -d "$SQL_AD" -U "$SQL_KULLANICI" -c "INSERT INTO $SQL_TABLO(event) VALUES ('TARIH $DEGISIKLIKLER');"
 
-echo $TARIH$DEGISIKLIKLER >>/home/projeKayit.txt
-fi
-sleep 5
+        echo $TARIH$DEGISIKLIKLER >>/home/projeKayit.txt
+    fi
+    sleep 5
 done
