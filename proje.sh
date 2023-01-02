7a78b94758d14413ab422b2cdd100def26f40379
zehrakr48 <zzehrakr48@gmail.com>
2023-01-02 13:37:48 +0300

    script yazıldı.

---
+++ proje.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DIZIN="/home/"
+SQL_AD="proje"
+SQL_KULLANICI="postres"
+SQL_SIFRE="0000"
+SQL_TABLO="table"
+
+while true; do
+TARIH=$(date)
+DEGİSİKLİKLER=$(inotifywait -e modify,move,create,delete --format'%w&f%e' -r $DIZIN)
+if[! -z "$DEGİSİKLİKLER"]; then
+psql -d "$SQL_AD" -U "$SQL_KULLANICI" -c "INSERT INTO $SQL_TABLO(event) VALUES ('TARIH $DEGISIKLIKLER');"
+
+echo $TARIH$DEGISIKLIKLER >>/home/projeKayit.txt
+fi
+sleep 5
+done
